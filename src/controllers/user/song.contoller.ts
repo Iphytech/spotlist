d3ac398e3d63616902b1a8d1e3361491e3c17293
Ifunanya Ikemma <ifavour902@gmail.com>
2022-08-09 17:20:06 +0100

    Removed irrelevant codes

--- src/controllers/user/song.contoller.ts
+++ src/controllers/user/song.contoller.ts
@@ -1,21 +1,15 @@
 import { Request, Response } from 'express';
 import crypto from 'crypto';
-import { NotFoundError } from '../../errors/not-found-error'
+import { BadRequestError } from '../../errors';
 import { createResponse, configurePaginationParams } from '../../utils/index';
-import { Song, User } from '../../models/index';
+import { Song } from '../../models/index';
 
 
 const getSingleUserList = async (req: Request, res: Response) => {
 
     const userId = req.currentUser["id"]
-    const existingUser = await User.findOne({ _id: userId })
-
-    if (!existingUser) {
-        throw new NotFoundError("User with the given ID not found");
-    }
     const totalDocumentCount = await Song.find({ user: userId }).countDocuments();
     configurePaginationParams(req, totalDocumentCount);
-
 
     const existingLists = await Song.find({ user: userId }).sort({ createdAt: -1 })
         .skip((req.pagination.currentPage - 1) * req.pagination.perPage)
@@ -26,19 +20,11 @@ const getSingleUserList = async (req: Request, res: Response) => {
 }
 
 const getSpecificListOfSingleUser = async (req: Request, res: Response) => {
-
-
     const listId = req.params.listid;
     const userId = req.currentUser["id"]
-    const existingUser = await User.findOne({ _id: userId })
-
-    if (!existingUser) {
-        throw new NotFoundError("User with the given ID not found");
-    }
 
     const existingUserList = await Song.find({ user: userId, listId: listId }).sort({ createdAt: -1 })
     return createResponse(res, 200, "User Lists successfully returned", existingUserList);
-
 
 }
 
@@ -47,12 +33,6 @@ const addListToUserRecord = async (req: Request, res: Response) => {
     const userId = req.currentUser["id"]
     const { songs } = req.body;
     const listId = crypto.randomBytes(3).toString('hex');;
-
-    const existingUser = await User.findOne({ _id: userId })
-
-    if (!existingUser) {
-        throw new NotFoundError("User with the given ID not found");
-    }
 
     const newList = Song.build({ user: userId, listId: listId, songs: songs });
     await newList.save();
@@ -71,7 +51,7 @@ const addSongToList = async (req: Request, res: Response) => {
     const userSong = await Song.findOne({ user: userId, listId: listId })
 
     if (!userSong) {
-        throw new NotFoundError("User with the given ID not found");
+        throw new BadRequestError("User is not the owner of the list or the list does not exist");
     }
 
     await Song.updateOne(
